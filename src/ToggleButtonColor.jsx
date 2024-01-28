6d864b3bcbc2baf318269f3bde0d4b998bf8dc9d
sunnymodi29 <modisunny25@gmail.com>
2024-01-28 15:44:02 +0530

    Initial Commit

---
+++ src/ToggleButtonColor.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const ToggleButtonColor = () => {
+
+    const [backgroundColor, setBackgroundColor] = useState("white");
+    const [textColor, setTextColor] = useState("#1b1b1b");
+    const [buttonStyle, setButtonStyle] = useState("white");
+
+    const handleClick = () => {
+        setBackgroundColor(backgroundColor === "white" ? "#1b1b1b" : "white");
+        setTextColor(textColor === "#1b1b1b" ? "#ffa31a" : "#1b1b1b");
+        setButtonStyle(backgroundColor === "white" ? "#1b1b1b" : "white");
+        document.body.style.backgroundColor = backgroundColor === "white" ? "#1b1b1b" : "white";
+    }
+
+    return (
+        <>
+            <section style={{ backgroundColor, color: textColor }}>
+                <button onClick={handleClick} style={{ buttonStyle, color: textColor, border: `2px solid ${textColor}` }}>{backgroundColor == "#1b1b1b" ? "Black Theme" : "White Theme"}</button>
+                <section className="content">
+                    <h1>Welcome To A <br />Real World</h1>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default ToggleButtonColor
